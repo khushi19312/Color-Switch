58a91dbe7f1297914fc39f00fabf9f463a195968
khushi19312 <57086221+khushi19312@users.noreply.github.com>
2020-11-29 16:20:17 +0530

    Update Ball.java

--- Ball.java
+++ Ball.java
@@ -1,5 +1,32 @@
 package ColorSwitch;
 
 public class Ball {
-
+  private int x_pos;
+  private int y_pos;
+  private int y_jump;
+  private int color;
+  private boolean presence;
+  private static long serialVersionUID;
+  
+  public int gety_pos(){
+    return x_pos;
+  }
+  public void sety_pos(int x){
+    y_pos = x;
+  }
+  public void increasey_jump(int y){
+    y_pos += y;
+  }
+  public void change_colour(int c){
+    color = c;
+  }
+  public int get_colour(){
+    return color;
+  }
+  public boolean get_presence(){
+    return presence;
+  }
+  public void change_presence(){
+    presence = !presence;
+  }
 }
