0e03fb198ba1321d5d296df2ff16988faa355aec
ananya2711 <54437908+ananya2711@users.noreply.github.com>
2020-12-01 06:17:48 +0530

    Update Game.java

--- Game.java
+++ Game.java
@@ -34,7 +34,7 @@ public class Game extends Application
 	//private Stars star_obj;
 	//private Stars next_nearest_star_obj;
 	private ColorWheel colorwheel_obj;
-	private HashMap<Integer,String> colors;
+	private HashMap<Integer,Color> colors;
 	private int currentScore;
 	private long serialVersionUID;
 	private static String[] args;
@@ -42,8 +42,18 @@ public class Game extends Application
 	Game(Stage stage) throws FileNotFoundException
 	{
 		ball_obj=new Ball();
-		start(stage);
 		
+		colors=new HashMap();
+		Color purple=Color.rgb(117, 37, 212);
+		Color pink=Color.rgb(255, 0, 128);
+		Color blue=Color.rgb(53, 226, 242);
+		Color yellow=Color.rgb(245, 223, 15);
+		colors.put(1,purple);//purple
+		colors.put(2,pink);//pink
+		colors.put(3,blue);//blue
+		colors.put(4,yellow);//yellow
+		
+		start(stage);
     }
 
 
@@ -233,7 +243,7 @@ public class Game extends Application
                 imView.setFitHeight(220);
                 imView.setFitWidth(220);
                 imView.setPreserveRatio(true);
-                System.out.println(index);
+               // System.out.println(index);
             }
         };
         animation3.play();
@@ -275,7 +285,13 @@ public class Game extends Application
         colorwheel.setPreserveRatio(true);
     }
 
-    
+    int getRandom()
+    {
+    	double f = Math.random()/Math.nextDown(1.0);
+        double x = 1*(1.0 - f) + 4*f;
+        return (int)x;
+
+    }
     public void start(Stage stage) throws FileNotFoundException {
 
         ImageView imageV=addbackground();
@@ -317,10 +333,10 @@ public class Game extends Application
         Image save = new Image("AP\\savebutton.png");
         ImageView sv = new ImageView(save);
         Button b2 = new Button();
-        b2 = setsavebutton(sv);
+        b2 = setsavebutton(sv);     
         addscaletransition(b2);
 
-        Circle ball = new Circle(10, Color.RED);
+        Circle ball = new Circle(9,colors.get(getRandom()));
 		ball_obj.setMyball(ball);
         ball.setCenterY(600);
         ball_obj.setY_pos((int)ball.getCenterY());
@@ -334,40 +350,15 @@ public class Game extends Application
         scene.addEventHandler(KeyEvent.KEY_PRESSED, (key) -> {
         	System.out.println("Enter pressed 1");
             if(key.getCode()==KeyCode.SHIFT) {
-            	 int maxtotravel=(int) ((ball.getCenterY()-ball_obj.getY_jump()));
-            	 if(ball_obj.pre!=null)
-            	 ball_obj.pre.stop();
-            	 ball_obj.dist=5;
-            	 ball_obj.gr=(float)0.2;
-            	System.out.println("Enter pressed");
-                 Timeline timeline2 = new Timeline(new KeyFrame(Duration.millis(10), new EventHandler<ActionEvent>(){
-                		 @Override
-                		 public void handle(ActionEvent e)
-                		 {
-                		ball.setCenterY(ball.getCenterY()-ball_obj.dist);
-                		ball_obj.setY_pos((int)ball.getCenterY());
-                		ball_obj.dist-=ball_obj.gr;
-                		//if(dist>0)System.out.println("************up***********");
-                		//else
-                			//System.out.println("down");
-                		if(ball.getCenterY()<=maxtotravel)
-                			{ball_obj.dist=-ball_obj.dist;
-                			//gr=-1*(float)0.1;
-                			}
+            	 System.out.println(ball.getCenterY());
+            	ball_obj.move();
                 			 
-                		 }
-                	 
-                  
-                 })); 
-                timeline2.setCycleCount(Animation.INDEFINITE);
-               // ball.setCenterY(ball.getCenterY()-20);
-              
-                timeline2.play(); 
-                ball_obj.pre=timeline2;
+            }
+                
                 
                 //System.out.println(ball.getCenterY());
                 
-            }  });
+              });
 
 		
 	
