3a108a87dcd7a6db8d8d0a2f774a6bbda98f200a
ananya2711 <54437908+ananya2711@users.noreply.github.com>
2020-12-02 13:20:32 +0530

    Update Obstacles.java

--- Obstacles.java
+++ Obstacles.java
@@ -1,15 +1,36 @@
 package application;
 
 import java.util.List;
+import javafx.animation.*;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.scene.Group;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
+import javafx.util.Duration;
 
-public class Obstacles {
-	private int x_pos;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList; 
+import java.util.List;
+import javafx.application.Application;
+
+public class Obstacles extends Application {
+	private int x_pos=570;
 	private int y_pos;
 	private String type;
 	private int orientation;
 	protected List <Integer> colors;
 	private int speed;
 	private static long serialVersionUID;
+	private ImageView obstacle;
 	
 	public void move() {
 		
@@ -24,7 +45,7 @@ public class Obstacles {
 		return x_pos;
 	}
 	public int gety_pos(){
-		return x_pos;
+		return y_pos;
 	}
 	public void setx_pos(int x) {
 		x_pos = x;
@@ -56,9 +77,19 @@ public class Obstacles {
 	public String getType() {
 		return type;
 	}
-	private void checkDifficulty(Stars s) {
+	//private void checkDifficulty(Stars s) {
+	@Override
+	public void start(Stage arg0) throws Exception {
+		// TODO Auto-generated method stub
 		
 	}
+	public ImageView getObstacle() {
+		return obstacle;
+	}
+	public void setObstacle(ImageView obstacle) {
+		this.obstacle = obstacle;
+	}
+		
+	//}
 
 }
-
