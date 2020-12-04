18e4d08747172ace00ba6506123feebfe3a3cfad
ananya2711 <54437908+ananya2711@users.noreply.github.com>
2020-12-04 18:03:17 +0530

    Update Game.java
    added object 3,4

--- Game.java
+++ Game.java
@@ -40,7 +40,7 @@ public class Game extends Application
 	private int currentScore;
 	private long serialVersionUID;
 	private static String[] args;
-	
+	boolean checking=false;
 
 	Game(Stage stage) throws Exception
 	{
@@ -65,7 +65,7 @@ public class Game extends Application
 		start(stage);
     }
 
-	 int getRandomColor()
+	 int getRandom()
 	    {
 	    	double f = Math.random()/Math.nextDown(1.0);
 	        double x = 1*(1.0 - f) + 4*f;
@@ -73,13 +73,7 @@ public class Game extends Application
 
 	    }
 	 
-	 int getRandom()
-	    {
-	    	double f = Math.random()/Math.nextDown(1.0);
-	        double x = 1*(1.0 - f) + 6*f;
-	        return (int)x;
-
-	    }
+	
 	public static int play()
 	{
 	
@@ -129,35 +123,24 @@ public class Game extends Application
 	
 	public void addObstacles() throws Exception
 	{
-		int prev_y = Obstacle_list.get(0).gety_pos()-300;
+		int prev_y = Obstacle_list.get(0).gety_pos()-500;
 		int no=getRandom();
-		System.out.println(no);
+		//System.out.println(no);
 		if(no==1){
 			Obstacle_list.add(new Obstacle1(prev_y));
-			System.out.print("added 1");
+			//System.out.print("added 1");
 		}
 		else if(no==2){
 			Obstacle_list.add(new Obstacle2(prev_y));
-			System.out.print("added 2");
+			//System.out.print("added 2");
 		}
-		else if(no==3){
+		else {
 			Obstacle_list.add(new Obstacle3(prev_y));
-			System.out.print("added 3");
+			//System.out.print("added 3");
 		}
-		else if(no==4){
-			Obstacle_list.add(new Obstacle4(prev_y));
-			System.out.print("added 4");
-		}
-		else if(no==5){
-			Obstacle_list.add(new Obstacle5(prev_y));
-			System.out.print("added 5");
-		}
-		else{
-			Obstacle_list.add(new Obstacle6(prev_y));
-			System.out.print("added 6");
-		};
 		
-		System.out.println("+++++++"+Obstacle_list.size());
+		
+		//System.out.println("+++++++"+Obstacle_list.size());
 		
 		
 	}
@@ -215,34 +198,33 @@ public class Game extends Application
         b2 = setsavebutton(sv);
         addscaletransition(b2);
         
-        
-        Circle ball = new Circle(9,colors.get(getRandomColor()));
+        int ballcolor=getRandom();
+        Circle ball = new Circle(12,colors.get(ballcolor));
 		ball_obj.setMyball(ball);
-        ball.setCenterY(700);
-        ball_obj.setY_pos((int)ball.getCenterY());
-        ball.setCenterX(738);
-        ball_obj.setX_pos((int)ball.getCenterX());
-        ball_obj.setY_jump(200);
+        ball_obj.sety_pos(700);
+        ball_obj.setx_pos(750);
+        ball_obj.change_colour(ballcolor);
         //ball.relocate(0,10);
         
         
     	
-        Group root = new Group(imageV, b1, b2, starthand, Obstacle_list.get(1).getObstacle(), ball, Obstacle_list.get(0).getObstacle());
+        Group root = new Group(imageV, b1, b2, starthand, Obstacle_list.get(1).getObstacle(), Obstacle_list.get(0).getObstacle(),ball);
         Scene scene = new Scene(root, 1500, 800);
 
-        System.out.println("Entering handler");
+       // System.out.println("Entering handler");
         scene.addEventHandler(KeyEvent.KEY_PRESSED, (key) -> {
-        	System.out.println("Enter pressed 1");
+        	//System.out.println("Enter pressed 1");
             if(key.getCode()==KeyCode.SHIFT) {
-            	System.out.println(ball.getCenterY());
+            	//System.out.println(ball.getCenterY());
             	ball_obj.move();
             	if(ball_obj.gety_pos()<700) {
-                	System.out.print("\nin for updation");
-            		starthand.setY(starthand.getY()+10);
-            		Obstacle_list.get(0).sety_pos(Obstacle_list.get(0).gety_pos()+10);
-            		Obstacle_list.get(1).sety_pos(Obstacle_list.get(1).gety_pos()+10);
+              //  	System.out.print("\nin for updation");
+            		starthand.setY(starthand.getY());
+            		Obstacle_list.get(0).sety_pos(Obstacle_list.get(0).gety_pos());
+            		Obstacle_list.get(1).sety_pos(Obstacle_list.get(1).gety_pos());
+            		ball_obj.sety_pos(ball_obj.gety_pos());
         		}
-            	if(Obstacle_list.get(0).gety_pos()>=700) {
+            	if(Obstacle_list.get(0).gety_pos()>=700 && checking==false) {
         			
         			root.getChildren().remove(Obstacle_list.get(0).getObstacle());
         			Obstacle_list.remove(0);
@@ -260,13 +242,46 @@ public class Game extends Application
 
         });
         //scene.setEventDispatcher(new EventDispatcher(Event e));
-          
+         
+			        Timeline checkExitCondition = new Timeline(new KeyFrame(Duration.millis(1),new EventHandler<ActionEvent>() {
+			
+			   @Override
+			   public void handle(ActionEvent event) {
+				   checking=true;
+			       if(ball_obj.gety_pos()>800)
+			    	   System.out.println("******************EXIT_OF_BOUNDS************************");
+			       for(int i=0;i<Obstacle_list.size();i++)
+			       {
+			    	   
+			    	   //System.out.println("Size: "+Obstacle_list.size()+" present "+i);
+			    	  int[] limits=Obstacle_list.get(i).collision_pos(ball_obj.gety_pos());
+			    	  //if(limits!=null)
+			    	  //System.out.println("length: "+limits.length);
+			    	  if(limits!=null && limits.length==2)
+			    	      {if(ball_obj.gety_pos()<=limits[0] && ball_obj.gety_pos()>=limits[1] && ball_obj.get_colour()!=Obstacle_list.get(i).getColours(ball_obj.gety_pos()))
+			    			  System.out.println("******************EXIT_LINE************************");}
+			    	  else if(limits!=null&& limits.length==4)
+			    	      {
+			    		  if(((ball_obj.gety_pos()<=limits[0] && ball_obj.gety_pos()>=limits[1]) ||(ball_obj.gety_pos()<=limits[2] && ball_obj.gety_pos()>=limits[3])) && ball_obj.get_colour()!=Obstacle_list.get(i).getColours(ball_obj.gety_pos()))
+			    			  System.out.println("******************EXIT_RING************************");
+			    		 // System.out.println("checking");
+			    	      }
+			    	    	  
+			    	  
+			       }
+			       
+			       checking=false;
+			   }
+			}));
+			checkExitCondition.setCycleCount(Timeline.INDEFINITE);
+			checkExitCondition.play();
         
         
         
         stage.setTitle("Color Switch");
         stage.setScene(scene);
-        stage.show();       
+        stage.show();   
+      
         
     }
     
