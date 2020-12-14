61cf782741f2ddcd283cbcfc33f0763f79e09117
khushi19312 <57086221+khushi19312@users.noreply.github.com>
2020-12-14 20:25:48 +0530

    Update Obstacle2.java

--- Obstacle2.java
+++ Obstacle2.java
@@ -12,6 +12,8 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.transform.Rotate;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
@@ -34,10 +36,11 @@ public class Obstacle2 extends Obstacles{
 	/*Obstacle1(Stage stage) throws Exception{
 		start(stage);
 	}*/
+	List<Rectangle> sq;
 	
 	Obstacle2(int pos) throws Exception
 	{
-		 
+		 sq= new ArrayList<Rectangle>();
          sety_pos(pos);
          start();
 	}
@@ -45,27 +48,71 @@ public class Obstacle2 extends Obstacles{
     public void start() throws Exception{
 
 
-        ImageView s = new ImageView();
-        List<Image> sq = new ArrayList<>();
-        
-        create(sq);
-        move(s, sq);
-        
-        setObstacle(s);
+        //ImageView s = new ImageView();
+    	//List<Rectangle> sq = new ArrayList<>(); 
+        create(sq);        
+        Group group = new Group(sq.get(0), sq.get(1), sq.get(2), sq.get(3));       
+        move(group, sq);         
+        setObstacle(group);
+        /*Group root = new Group(group);
+        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.show();*/
 
     }
 
-    public void create(List<Image> sq) throws FileNotFoundException {
-    	Image square1 = new Image("AP\\square1.png");
+    public void create(List<Rectangle> sq) throws FileNotFoundException {
+    	/*Image square1 = new Image("AP\\square1.png");
         Image square2 = new Image("AP\\square2.png");
         Image square3 = new Image("AP\\square3.png");
         Image square4 = new Image("AP\\square4.png");
         Image square5 = new Image("AP\\square5.png");
         Image square6 = new Image("AP\\square6.png");
         Image square7 = new Image("AP\\square7.png");
-        Image square8 = new Image("AP\\square8.png");
+        Image square8 = new Image("AP\\square8.png");*/
+        
+    	
+    	Rectangle rect1= new Rectangle();
+        rect1.setX(650); 
+		rect1.setY(gety_pos()-80); 
+		rect1.setWidth(200); 
+		rect1.setHeight(20); 
+		rect1.setArcWidth(30.0); 
+		rect1.setArcHeight(20.0);  
+		rect1.setFill(Color.web("#FF0181"));
+        
+		Rectangle rect2= new Rectangle();
+		rect2.setX(650); 
+		rect2.setY(gety_pos()-80); 
+		rect2.setWidth(20); 
+		rect2.setHeight(200); 
+		rect2.setArcWidth(30.0); 
+		rect2.setArcHeight(20.0);  
+		rect2.setFill(Color.web("#900DFF"));
+		
+        Rectangle rect3= new Rectangle();
+        rect3.setX(650); 
+		rect3.setY(gety_pos()+100); 
+		rect3.setWidth(200); 
+		rect3.setHeight(20); 
+		rect3.setArcWidth(30.0); 
+		rect3.setArcHeight(20.0);  
+		rect3.setFill(Color.web("FAE100"));
+        
+        Rectangle rect4= new Rectangle();
+        rect4.setX(830); 
+		rect4.setY(gety_pos()-80); 
+		rect4.setWidth(20); 
+		rect4.setHeight(200); 
+		rect4.setArcWidth(30.0); 
+		rect4.setArcHeight(20.0);  
+		rect4.setFill(Color.web("#32DBF0"));
+        
+		sq.add(rect1);
+        sq.add(rect2);
+        sq.add(rect3);
+        sq.add(rect4);
 
-        sq.add(square1);
+        /*sq.add(square1);
         sq.add(square2);
         sq.add(square3);
         sq.add(square4);
@@ -73,35 +120,25 @@ public class Obstacle2 extends Obstacles{
         sq.add(square6);
         sq.add(square7);
         sq.add(square8);
-        sq.add(square8);
+        sq.add(square8);*/
 
     }
 
-    public void move(ImageView s, List<Image> sq) {
+    public void move(Group g, List<Rectangle> sq) {
 
-    	Transition squarerotate = new Transition() {
-            {
-                setCycleDuration(Duration.millis(2000));
-                setCycleCount(Animation.INDEFINITE);
-            }
-
-            @Override
-            protected void interpolate(double fraction) {
-                int index = (int) (fraction*(sq.size()-1));
-                s.setImage(sq.get(index));
-                setOrientation(index);
-                s.setX(getx_pos()+100);
-                s.setY(gety_pos());
-                s.setFitHeight(280);
-                s.setFitWidth(280);
-                s.setPreserveRatio(true);
-            }
-
-        };
-        squarerotate.play();
+    	RotateTransition rotate = new RotateTransition(); 
+        
+        rotate.setDuration(Duration.millis(4000));
+        rotate.setCycleCount(Animation.INDEFINITE);
+    	rotate.setByAngle(360);
+    	rotate.setAutoReverse(false);
+    	rotate.setInterpolator(Interpolator.LINEAR);
+    	//rotate.pivotXProperty().bind(195);
+    	//rotate.pivotYProperty().bind(400);
+    	rotate.setNode(g);
+    	rotate.play();
     }
 
-	
 	
 	public void get_ypos() {
 		
@@ -118,5 +155,8 @@ public class Obstacle2 extends Obstacles{
 		// TODO Auto-generated method stub
 		return 0;
 	}
+
+		
+	
 	
 }
